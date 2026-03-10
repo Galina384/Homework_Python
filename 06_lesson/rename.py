a4719c7c850045e6a972c978a632662183001427
Galina384 <kasatkina.galina2018@yandex.ru>
2026-03-10 15:14:50 +0500

    commit_6

---
+++ 06_lesson/rename.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install())
+)
+driver.maximize_window()
+driver.get("http://uitestingplayground.com/textinput")
+
+driver.find_element(By.ID, "newButtonName").send_keys("SkyPro")
+driver.find_element(By.ID, "updatingButton").click()
+
+text = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.ID, "updatingButton"))
+).text
+
+print(text)
+driver.quit()
